e8e5146551b1dbea0eaf97f87be3a9492de818d1
Waqas Munir <devwaqas232@gmail.com>
2024-08-19 17:00:38 +0500

    Completed Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,57 @@
+'use client';
+
+import Link from 'next/link';
+import { CiSearch, CiShoppingCart, CiHeart } from 'react-icons/ci';
+import { FaRegCircleUser } from 'react-icons/fa6';
+
+import { Button } from './ui/button';
+
+export default function Navbar() {
+  const isAuth = true;
+
+  return (
+    <nav className="flex justify-between items-center md:px-16 px-8 h-20 gap-8">
+      <div className="font-semibold text-xl">
+        <Link href="/">MyShop</Link>
+      </div>
+      <div className="w-3/5">
+        <div className="flex justify-between px-6 py-2 rounded-md items-center bg-[#F5F5F5] ">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-[95%]"
+          />
+          <CiSearch className="text-black" size={24} />
+        </div>
+      </div>
+      <div className="md:w-1/5 w-auto">
+        <div className="flex justify-center items-center md:gap-8 gap-4">
+          <div className="relative">
+            <Button variant="icon" size="icon" className="absolute right-0">
+              0
+            </Button>
+            <CiShoppingCart
+              color="black"
+              size={40}
+              className="cursor-pointer"
+            />
+          </div>
+          {isAuth ? (
+            <div className="flex justify-center items-center md:gap-8 gap-4">
+              <CiHeart color="black" size={32} className="cursor-pointer" />
+              <FaRegCircleUser
+                color="black"
+                size={28}
+                className="cursor-pointer"
+              />
+            </div>
+          ) : (
+            <Button variant="secondary" size="lg">
+              Login
+            </Button>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
